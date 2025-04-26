8d1964868acea2e3ecdea8b68522e2fde202255b
Misael Gaytan <gaytanmisael@gmail.com>
2025-04-26 12:41:57 -0700

    misael:Made some progress

---
+++ src/app/(app)/_components/app-sidebar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  ArrowUpCircleIcon,
+  BarChartIcon,
+  CameraIcon,
+  ClipboardListIcon,
+  DatabaseIcon,
+  FileCodeIcon,
+  FileIcon,
+  FileTextIcon,
+  FolderIcon,
+  HelpCircleIcon,
+  LayoutDashboardIcon,
+  ListIcon,
+  SearchIcon,
+  SettingsIcon,
+  UsersIcon,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <ArrowUpCircleIcon className="h-5 w-5" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+    </Sidebar>
+  );
+}
--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,8 @@
 import { AuthProvider } from "@/context/AuthProvider";
 import { redirect } from "next/navigation";
 
+import { AppSidebar } from "./_components/app-sidebar";
+import { SiteHeader } from "./_components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 import { getCurrentSession } from "@/server/auth/session";
@@ -16,7 +18,19 @@ export default async function AppLayout({
   return (
     <AuthProvider initialData={initData}>
       <SidebarProvider>
-        <SidebarInset>{children}</SidebarInset>
+        <AppSidebar />
+        <SidebarInset>
+          <main className="bg-background relative flex w-full flex-1 flex-col md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2">
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </main>
+        </SidebarInset>
       </SidebarProvider>
     </AuthProvider>
   );
